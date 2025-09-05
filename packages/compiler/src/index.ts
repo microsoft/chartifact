8f409988e3871e42bc3452847ef881edb6ca7947
Dan Marshall <danmar@microsoft.com>
2025-09-05 09:40:36 -0700

    Export validation module from validate/index.js

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -2,4 +2,5 @@
 * Copyright (c) Microsoft Corporation.
 * Licensed under the MIT License.
 */
+export * as validation from './validate/index.js';
 export { targetMarkdown, normalizeNewlines } from './md.js';
