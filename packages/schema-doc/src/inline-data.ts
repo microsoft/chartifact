06ca7aecb7a9b4a208c2cb167d2cb92ab1ceb0c4
Dan Marshall <danmar@microsoft.com>
2025-11-20 17:38:58 -0800

    remove comments

--- packages/schema-doc/src/inline-data.ts
+++ packages/schema-doc/src/inline-data.ts
@@ -4,27 +4,18 @@
 */
 import { VariableID, ElementBase } from './common.js';
 
-/**
- * CSV data in markdown fence: ```csv variableId
- */
 export interface CsvElement extends ElementBase {
   type: 'csv';
   variableId: VariableID;
   content: string | string[] | string[][];
 }
 
-/**
- * TSV data in markdown fence: ```tsv variableId
- */
 export interface TsvElement extends ElementBase {
   type: 'tsv';
   variableId: VariableID;
   content: string | string[] | string[][];
 }
 
-/**
- * Custom delimiter-separated data in markdown fence: ```dsv delimiter:| variableId
- */
 export interface DsvElement extends ElementBase {
   type: 'dsv';
   variableId: VariableID;
@@ -32,18 +23,12 @@ export interface DsvElement extends ElementBase {
   content: string | string[] | string[][];
 }
 
-/**
- * JSON data with type preservation in markdown fence: ```json value variableId
- */
 export interface JsonValueElement extends ElementBase {
   type: 'json';
   variableId: VariableID;
   content: object | object[];
 }
 
-/**
- * YAML data with type preservation in markdown fence: ```yaml value variableId
- */
 export interface YamlValueElement extends ElementBase {
   type: 'yaml';
   variableId: VariableID;
